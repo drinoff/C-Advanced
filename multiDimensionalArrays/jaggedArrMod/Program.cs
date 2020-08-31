e835a2e45d8136938e9efc1926d60c0923db5583
drinoff <drinoff@gmail.com>
2020-08-31 23:29:04 +0200

    update

---
+++ multiDimensionalArrays/jaggedArrMod/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace jaggedArrMod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rows = int.Parse(Console.ReadLine());
+            var jagged = new int[rows][];
+            for (int row = 0; row < rows; row++)
+            {
+                jagged[row] = ParseInput();
+            }
+            var input = Console.ReadLine().ToLower();
+            while (input != "end")
+            {
+                var command = input.Split();
+                var action = command[0];
+                var row = int.Parse(command[1]);
+                var col = int.Parse(command[2]);
+                var value = int.Parse(command[3]);
+                if (row > rows - 1 || row < 0 || col < 0 || col > jagged[row].Length)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+                else
+                {
+                    if (action == "add")
+                    {
+                        jagged[row][col] += value;
+                    }
+                    else
+                    {
+                        jagged[row][col] -= value;
+                    }
+                }
+                input = Console.ReadLine().ToLower();
+            }
+            for (int row = 0; row < jagged.Length; row++)
+            {
+                Console.WriteLine(string.Join(" ", jagged[row]));
+            }
+
+        }
+        static int[] ParseInput()
+        {
+            return Console.ReadLine().Split().Select(int.Parse).ToArray();
+        }
+        
+    }
+}
---
+++ multiDimensionalArrays/symbolInMatrix/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data.Common;
+using System.Linq;
+
+namespace symbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = int.Parse(Console.ReadLine());
+            var rows = size;
+            var cols = size;
+            var matrix = new char[rows, cols];
+            MatrixFiller(rows, cols, matrix);
+            var symbol = char.Parse(Console.ReadLine());
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    if(matrix[row,col] == symbol)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        System.Environment.Exit(0);
+                    }
+                }
+            }
+            Console.WriteLine($"{symbol} does not occur in the matrix ");
+            
+        }
+        static char[,] MatrixFiller(int rows, int cols, char[,] matrix)
+        {
+
+            for (int row = 0; row < rows; row++)
+            {
+                var currentRow = ParseInput(' ');
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+            return matrix;
+        }
+        static char[] ParseInput(params char[] separator)
+        {
+            return Console.ReadLine().ToCharArray();
+        }
+    }
+}
