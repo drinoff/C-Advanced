b72fc5fd2af2babe265a918b20879d02492b63ec
drinoff <drinoff@gmail.com>
2020-09-07 21:30:12 +0200

    update

--- setsAndDicts/averageStudentsGrade/Program.cs
+++ setsAndDicts/averageStudentsGrade/Program.cs
@@ -9,22 +9,22 @@ namespace averageStudentsGrade
         static void Main(string[] args)
         {
             var count = int.Parse(Console.ReadLine());
-            var dict = new Dictionary<string, List<double>>();
+            var dict = new Dictionary<string, List<decimal>>();
             for (int i = 0; i < count; i++)
             {
                 var input = InputParser();
                 var name = input[0];
-                var grade = double.Parse(input[1]);
+                var grade = decimal.Parse(input[1]);
 
                 if(!dict.ContainsKey(name))
                 {
-                    dict.Add(name, new List<double>());
+                    dict.Add(name, new List<decimal>());
                 }
                     dict[name].Add(grade);
             }
             foreach (var student in dict)
             {
-                Console.WriteLine($"{student.Key} -> {string.Join(" ",student.Value)} (avg: {student.Value.Average():f2})");
+                Console.WriteLine($"{student.Key} -> {string.Join(" ",student.Value.Select(x=>x.ToString("f2")))} (avg: {student.Value.Average():f2})");
             }
 
         }
---
+++ setsAndDicts/citiesByContinentAndCountry/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace citiesByContinentAndCountry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            var count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                var input = Console.ReadLine().Split();
+                var continent = input[0];
+                var country = input[1];
+                var city = input[2];
+                if (!dict.ContainsKey(continent))
+                {
+                    dict.Add(continent, new Dictionary<string, List<string>>());
+                }
+                if (!dict[continent].ContainsKey(country))
+                {
+                    dict[continent].Add(country, new List<string>());
+                }
+
+
+                dict[continent][country].Add(city);
+
+            }
+            foreach (var cont in dict)
+            {
+                Console.WriteLine($"{cont.Key}:");
+                foreach (var countr in cont.Value)
+                {
+                    Console.WriteLine($"{countr.Key} -> {string.Join(", ", countr.Value)}");
+                }
+            }
+
+        }
+    }
+}
