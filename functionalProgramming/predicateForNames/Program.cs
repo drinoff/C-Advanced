6266f127fec773184139f09a838e229155641885
Drinoff <drinoff@gmail.com>
2020-10-05 22:01:16 +0200

    up

---
+++ functionalProgramming/predicateForNames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace predicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filter = int.Parse(Console.ReadLine());
+            Func<string, bool> nameFilter = x => x.Length <= filter;
+            Console.ReadLine()
+                   .Split()
+                   .Where(nameFilter)
+                   .ToList()
+                   .ForEach(Console.WriteLine);
+                               
+                           
+        }               
+    }
+}
---
+++ functionalProgramming/reverseAndExclude/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace reverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var numbers = Console.ReadLine()
+                                 .Split()
+                                 .Select(int.Parse)
+                                 .Reverse()
+                                 .ToArray();
+
+            var n = int.Parse(Console.ReadLine());
+
+            Func<int,bool> command = x =>  x % n != 0;
+            var numbers2 = numbers.Where(command);
+                                 
+            Console.WriteLine(string.Join(" ",numbers2));
+        }
+    }
+}
