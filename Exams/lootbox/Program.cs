d699457e27e287e97c7da74e21c6c33858bea707
Drinoff <drinoff@gmail.com>
2020-10-27 13:18:41 +0100

    up

---
+++ Exams/lootbox/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace lootbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lootBoxOne = new Queue<int>(InputParser());
+            var lootBoxTwo = new Stack<int>(InputParser());
+            var itemCollection = new List<int>();
+
+            while (!(lootBoxOne.Count == 0 || lootBoxTwo.Count == 0))
+            {
+                var currBoxOne = lootBoxOne.Peek();
+                var currBoxTwo = lootBoxTwo.Peek();
+                var sumBox = currBoxOne + currBoxTwo;
+                if (IsEven(sumBox))
+                {
+                    itemCollection.Add(sumBox);
+                    lootBoxOne.Dequeue();
+                    lootBoxTwo.Pop();
+                }
+                else
+                {
+                    lootBoxOne.Enqueue(currBoxTwo);
+                    lootBoxTwo.Pop();
+                }
+            }
+            if(!lootBoxOne.Any())
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+            if(itemCollection.Sum()>=100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {itemCollection.Sum()}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {itemCollection.Sum()}");
+            }
+        }
+        public static int[] InputParser() => Console.ReadLine()
+                                                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                                    .Select(int.Parse)
+                                                    .ToArray();
+        public static bool IsEven(int sumBox) => sumBox % 2 == 0;
+
+        
+    }
+}
---
+++ Exams/reVolt/Program.cs
@@ -0,0 +1,270 @@
+﻿
+using System;
+using System.IO;
+
+namespace bee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fieldSize = int.Parse(Console.ReadLine());
+            var commandCount = int.Parse(Console.ReadLine());
+            var rows = fieldSize;
+            var cols = fieldSize;
+            var field = new char[rows, cols];
+
+            var playerRow = 0;
+            var playerCol = 0;
+            for (int row = 0; row < rows; row++)
+            {
+                var currRow = Console.ReadLine();
+                for (int col = 0; col < currRow.Length; col++)
+                {
+                    field[row, col] = currRow[col];
+                    if (field[row, col] == 'f')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
+                }
+            }
+            var edge = false;
+            var finished = false;
+
+            for (int i = 0; i < commandCount; i++)
+            {
+                var command = Console.ReadLine();
+                switch (command)
+                {
+                    case "up":
+                        if (playerRow - 1 < 0)
+                        {
+                            field[playerRow, playerCol] = '-';
+                            playerRow = field.GetLength(0) - 1;
+                            edge = true;
+                            field[playerRow, playerCol] = 'f';
+                        }
+                        if (edge)
+                        {
+                            edge = false;
+                        }
+
+                        else
+                        {
+                            field[playerRow, playerCol] = '-';
+                            playerRow -= 1;
+                        }
+
+                        if (field[playerRow, playerCol] == 'F')
+                        {
+                            field[playerRow, playerCol] = 'f';
+                            finished = true;
+                            break;
+                        }
+
+                        else if (field[playerRow, playerCol] == 'B')
+                        {
+                            //field[playerRow, playerCol] = '-';
+                            if (playerRow - 1 < 0)
+                            {
+                                
+                                playerRow = field.GetLength(0) - 1;
+                                edge = true;
+                                field[playerRow, playerCol] = 'f';
+                            }
+                            playerRow -= 1;
+                            if (field[playerRow, playerCol] == 'F')
+                            {
+                                field[playerRow, playerCol] = 'f';
+                                finished = true;
+                                break;
+                            }
+                            
+                        }
+
+                        else if (field[playerRow, playerCol] == 'T')
+                        {
+                            playerRow += 1;
+                        }
+
+                        else
+                        {
+
+                            field[playerRow, playerCol] = 'f';
+                        }
+
+                        break;
+                    case "down":
+                        if (playerRow + 1 > field.GetLength(0) - 1)
+                        {
+                            field[playerRow, playerCol] = '-';
+                            playerRow = 0;
+                            edge = true;
+                        }
+                        if (edge)
+                        {
+                            edge = false;
+                        }
+
+                        else
+                        {
+                            field[playerRow, playerCol] = '-';
+                            playerRow += 1;
+                        }
+
+                        if (field[playerRow, playerCol] == 'F')
+                        {
+                            field[playerRow, playerCol] = 'f';
+                            finished = true;
+                            break;
+                        }
+
+                        else if (field[playerRow, playerCol] == 'B')
+                        {
+                            //field[playerRow, playerCol] = '-';
+                            playerRow += 1;
+                            if (field[playerRow, playerCol] == 'F')
+                            {
+                                field[playerRow, playerCol] = 'f';
+                                finished = true;
+                                break;
+                            }
+                        }
+
+                        else if (field[playerRow, playerCol] == 'T')
+                        {
+                            playerRow -= 1;
+                        }
+                        else
+                        {
+
+                            field[playerRow, playerCol] = 'f';
+                        }
+
+
+                        break;
+                    case "right":
+                        if (playerCol + 1 > field.GetLength(1) - 1)
+                        {
+                            field[playerRow, playerCol] = '-';
+                            playerCol = 0;
+                            edge = true;
+                        }
+                        if (edge)
+                        {
+                            edge = false;
+                        }
+
+                        else
+                        {
+                            field[playerRow, playerCol] = '-';
+                            playerCol += 1;
+                        }
+
+                        if (field[playerRow, playerCol] == 'F')
+                        {
+                            field[playerRow, playerCol] = 'f';
+                            finished = true;
+                            break;
+                        }
+
+                        else if (field[playerRow, playerCol] == 'B')
+                        {
+                            //field[playerRow, playerCol] = '-';
+                            playerCol += 1;
+                            if (field[playerRow, playerCol] == 'F')
+                            {
+                                field[playerRow, playerCol] = 'f';
+                                finished = true;
+                                break;
+                            }
+                        }
+
+                        else if (field[playerRow, playerCol] == 'T')
+                        {
+                            playerCol -= 1;
+                        }
+                        else
+                        {
+
+                            field[playerRow, playerCol] = 'f';
+                        }
+                        break;
+                    case "left":
+                        if (playerCol - 1 < 0)
+                        {
+                            field[playerRow, playerCol] = '-';
+                            playerRow = field.GetLength(1) - 1;
+                            edge = true;
+                        }
+                        if (edge)
+                        {
+                        }
+
+                        else
+                        {
+                            field[playerRow, playerCol] = '-';
+                            playerCol -= 1;
+                        }
+
+                        if (field[playerRow, playerCol] == 'F')
+                        {
+                            field[playerRow, playerCol] = 'f';
+                            finished = true;
+                            break;
+                        }
+
+                        else if (field[playerRow, playerCol] == 'B')
+                        {
+                            //field[playerRow, playerCol] = '-';
+                            if (playerCol - 1 < 0)
+                            {
+                                
+                                playerCol = field.GetLength(1) - 1;
+                                edge = true;
+                            }
+                            playerCol -= 1;
+                            if (field[playerRow, playerCol] == 'F')
+                            {
+                                field[playerRow, playerCol] = 'f';
+                                finished = true;
+                                break;
+                            }
+                        }
+
+                        else if (field[playerRow, playerCol] == 'T')
+                        {
+                            playerCol += 1;
+                        }
+                        else
+                        {
+
+                            field[playerRow, playerCol] = 'f';
+                        }
+                        break;
+                }
+                if (finished)
+                {
+                    break;
+                }
+            }
+            if (finished)
+            {
+                Console.WriteLine("Player won!");
+            }
+            else
+            {
+                Console.WriteLine("Player lost!");
+            }
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    Console.Write(field[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
